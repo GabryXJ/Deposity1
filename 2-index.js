758dbc8a5b253c5b18513112084bfa40fe237937
GabryXJ <115703263+GabryXJ@users.noreply.github.com>
2022-11-03 12:52:59 +0100

    Add files via upload

---
+++ 2-index.js
@@ -0,0 +1,10 @@
+console.log("TEST 2022-11-03")
+
+let stringaacapo = "la \n \"mia stringa\""
+console.log("stringa a capo ->", stringaacapo)
+
+let stringa = "la \\ \"mia stringa\""
+console.log("stringa ->",stringa)
+
+
+
